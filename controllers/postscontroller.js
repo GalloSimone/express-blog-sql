00f4ed6110bfcf4503bc16bc1f9ae5c8d63c03b8
GalloSimone <gallosimonecz@gmail.com>
2025-01-09 15:43:01 +0100

    milestone 1 completata

--- controllers/postscontroller.js
+++ controllers/postscontroller.js
@@ -1,8 +1,9 @@
-const posts = require("../data/posts");
+const connection = require('../db/db.js');
 
 
 
 //INDEX
+
 function index (req,res) {
     res.json({message:"lista dei post", posts, postsCount: posts.length})
 }
@@ -11,49 +12,49 @@ function index (req,res) {
 function show (req,res) {
     const id = parseInt(req.params.id);
     const post =posts.find((post) =>post.id===id);
-     res.json( post )
+    res.json( post )
 }
 
 //STORE
     function store (req,res){
     const {titolo, description, img, avaible,tags}=req.body;
-    const id=posts.at(-1).id+1
+   const id=posts.at(-1).id+1
     const newPost={
-    id:id,
-    titolo:titolo,
-    description:description,
-    img:img,
+     id:id,
+      titolo:titolo,
+      description:description,
+     img:img,
     avaible:avaible,
-    tags:tags
-    }
-    posts.push(newPost)
+     tags:tags
+     }
+     posts.push(newPost)
 
-    res.json(newPost)
-}
+     res.json(newPost)
+ }
 
 //UPDATE
-function update (req,res){
-    const id = parseInt (req.params.id);
-    const post =posts.find((post) =>post.id===id);
-    const {titolo, description, img,avaible, tags}=req.body;
+ function update (req,res){
+     const id = parseInt (req.params.id);
+     const post =posts.find((post) =>post.id===id);
+     const {titolo, description, img,avaible, tags}=req.body;
    
         
-        post.titolo=titolo;
+       post.titolo=titolo;
         post.description=description;
-        post.img=img;
-        post.avaible=avaible
-        post.tags=tags;
+       post.img=img;
+     post.avaible=avaible
+      post.tags=tags;
         res.json(post)
-}
+ }
 //DESTROY
-function destroy (req,res){
-const id = parseInt (req.params.id);
-const eliminato =posts.find((post,index) =>post.id===id);
-console.log( "Elemento eliminato: ", eliminato);
-posts.splice(posts.indexOf(eliminato), 1);
-res.sendStatus(204);
-console.log("Lista aggiornata: ", posts);
-}
+ function destroy (req,res){
+ const id = parseInt (req.params.id);
+  const eliminato =posts.find((post,index) =>post.id===id);
+  console.log( "Elemento eliminato: ", eliminato);
+ posts.splice(posts.indexOf(eliminato), 1);
+ res.sendStatus(204);
+ console.log("Lista aggiornata: ", posts);
+ }
 
-module.exports = {index,show,store,update,destroy};
+ module.exports = {index,show,store,update,destroy};
 
--- routers/posts.js
+++ routers/posts.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const posts =require("../data/posts.js")
+
 const postsController = require("../controllers/postscontroller.js")
 
 
