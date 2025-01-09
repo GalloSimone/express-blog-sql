dd29531d675228d5345ddb40310bdb1ad2e67c80
GalloSimone <gallosimonecz@gmail.com>
2025-01-09 16:36:08 +0100

    milestone 4 completata

--- controllers/postscontroller.js
+++ controllers/postscontroller.js
@@ -21,8 +21,24 @@ function index (req,res) {
 //SHOW
 function show (req,res) {
     const id = parseInt(req.params.id);
-    const post =posts.find((post) =>post.id===id);
-    res.json( post )
+    const sql ='SELECT * FROM `posts` WHERE `id` = ?';
+    connection.query(sql,[id],(err,results)=>{
+
+ if (err) {
+    console.log(err);
+    return res.status(500).json({error:'Database query failed'});
+ }
+ if(results.length===0){
+    return res.status(404).json({error:'post not fund'});
+ }
+let post =results[0];
+
+res.json((results))
+    })
+
+
+    /**const post =posts.find((post) =>post.id===id);
+    res.json( post )*/
 }
 
 //STORE
